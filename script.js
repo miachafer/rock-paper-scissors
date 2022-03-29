3c2f215db002e4e766d4db7eb2506a1b5867c583
miachafer <miachafer@gmail.com>
2022-03-29 20:06:40 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,92 @@
+/*
+Function the makes the computer chooses randomly a number
+1 = rock
+2 = paper
+3 = scissors 
+*/
+
+function computerPlays() {
+    let randomChoice = Math.round(Math.random() * 2 + 1);
+    switch (randomChoice) {
+        case 1:
+            computerChoice = "rock";
+            break
+        case 2:
+            computerChoice = "paper";
+            break
+        case 3:
+            computerChoice = "scissors";
+    }
+    return computerChoice;
+}
+
+function userPlays() {
+    let userChoice = (prompt("Rock-Paper-Scissors?")).toLowerCase();
+    return userChoice;
+}
+
+function playRound() {
+
+    let computerChoice = computerPlays();
+    let userChoice = userPlays();
+
+    if (userChoice == "rock") {
+        if (computerChoice == "paper") {
+            console.log("You lose! Paper beats rock.");
+            return "User lose" 
+        } else if (computerChoice == "scissors") {
+            console.log("You win! Rock beats scissors.");
+            return "User win"
+        } else {
+            console.log(`Tie! You and the computer chose ${userChoice}!\nBoth get 1 point.`);
+            return "Tie"
+        }
+    } else if (userChoice == "paper") {
+        if (computerChoice == "rock") {
+            console.log("You win! Paper beats rock.");
+            return "User win"
+        } else if (computerChoice == "scissors") {
+            console.log("You lose! Scissors beats paper.")
+            return "User lose"
+        } else {
+            console.log(`Tie! You and the computer chose ${userChoice}!\nBoth get 1 point.`);
+            return "Tie"
+        }
+    } else if (userChoice == "scissors") {
+        if (computerChoice == "rock") {
+            console.log("You lose! Rock beats scissors.")
+            return "User lose"
+        } else if (computerChoice == "paper") {
+            console.log("You win! Scissors beats paper.")
+            return "User win"
+        } else {
+            console.log(`Tie! You and the computer chose ${userChoice}!\nBoth get 1 point.`);
+            return "Tie"
+        }
+    }
+
+}
+
+function game() {
+    let computerScore = 0;
+    let userScore = 0;
+    for (let i = 0; i < 5; i++) {
+        result = playRound(); // here we get the return values!
+        if (result.includes("Tie")) {
+            computerScore++
+            userScore++
+        } else if (result.includes("win")) {
+            userScore++
+        } else if (result.includes("lose")) {
+            computerScore++
+        }
+        console.log(`SCORE\nYou: ${userScore}\nComputer: ${computerScore}`)
+    } 
+    if (userScore > computerScore) {
+        console.log("YOU WIN");
+    } else if (userScore < computerScore){
+        console.log("YOU LOSE")
+    } else {
+        console.log("TIE")
+    }
+}
